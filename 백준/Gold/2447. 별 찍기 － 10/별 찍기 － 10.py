8eed8dd6e2e89e2fabcfb733c8702f1709cf5093
iamdudumon <102767676+iamdudumon@users.noreply.github.com>
2023-07-25 16:33:05 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 332 ms, Memory: 43572 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -1,29 +1,21 @@
 
 def draw_star(n, row, col):
     if n == 3 and row < size and col <size:
-       str_list[row][col] = "*"
-       str_list[row][col+1] = "*"
-       str_list[row][col+2] = "*"
-       str_list[row+1][col] = "*"
-       #str_list[row+1][col+1] = " "
-       str_list[row+1][col+2] = "*"
-       str_list[row+2][col] = "*"
-       str_list[row+2][col+1] = "*"
-       str_list[row+2][col+2] = "*"
+       str_list[row][col] = "***"
+       str_list[row+1][col] = "* *"
+       str_list[row+2][col] = "***"
     else:
         draw_star(n/3, int(row), int(col))
         draw_star(n/3, int(row + n/3), int(col))
         draw_star(n/3, int(row + n/3 * 2), int(col))
-        draw_star(n/3, int(row), int(col + n/3))
-        #draw_star(n/3, int(row + n/3), int(col + n/3))
-        draw_star(n/3, int(row + n/3 * 2), int(col + n/3))
-        draw_star(n/3, int(row), int(col + n/3 * 2))
-        draw_star(n/3, int(row + n/3), int(col + n/3 * 2))
-        draw_star(n/3, int(row + n/3 * 2), int(col + n/3 * 2))
+        draw_star(n/3, int(row), int(col + n/9))
+        draw_star(n/3, int(row + n/3 * 2), int(col + n/9))
+        draw_star(n/3, int(row), int(col + n/9 * 2))
+        draw_star(n/3, int(row + n/3), int(col + n/9 * 2))
+        draw_star(n/3, int(row + n/3 * 2), int(col + n/9 *2))
     
-
 n = int(input())
-str_list = [[" " for _ in range(n)] for _ in range(n)]
+str_list = [["   " for _ in range(int(n/3))] for _ in range(n)]
 size = len(str_list)
 draw_star(n, 0, 0)
 for idx in range(len(str_list)):
