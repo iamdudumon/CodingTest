1b7a317909b42b367550c9f7cbdffd5bab62641c
iamdudumon <102767676+iamdudumon@users.noreply.github.com>
2023-08-03 12:31:51 +0900

    [Silver V] Title: 돌 게임, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.py
+++ 백준/Silver/9655. 돌 게임/돌 게임.py
@@ -1,3 +1,15 @@
+# dp 이용
 n = int(input())
 
-print("SK" if not n % 2 == 0 else "CY")
+dp = [0] * 1001  # dp[n] 은 n 개의 돌을 가지고 게임할 때 진행할 게임 횟수
+dp[0] = 0
+dp[1] = 1
+dp[2] = 2
+
+for idx in range(3, n + 1):
+    dp[idx] = min(dp[idx - 1], dp[idx - 3]) + 1
+
+if not dp[n] % 2 == 0:
+    print("SK")
+else:
+    print("CY")
