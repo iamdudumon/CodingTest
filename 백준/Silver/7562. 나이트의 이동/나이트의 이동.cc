29b081bf4954f27e465f506365ed61ebaf96b4c9
김두현 <102767676+iamdudumon@users.noreply.github.com>
2023-12-02 01:22:39 +0900

    [Silver I] Title: 나이트의 이동, Time: 52 ms, Memory: 9668 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <queue>
-#include <set>
 #include <utility>
 
 using namespace std;
@@ -32,10 +31,8 @@ int main() {
 	for (int i = 0; i < test; i++) {
 		int I;
 		int **graph;
-		set<pair<int, int>> visited;
 
 		cin >> I;
-
 
 		graph = new int*[I];
 		for (int j = 0; j < I; j++) {
@@ -57,39 +54,27 @@ int main() {
 		queue.push(start);
 
 		bool state = false;
-		while (true) {
+		while (!queue.empty()) {
 			pair<int, int> position = queue.front();
-			//cout << queue.front().first << ", " << queue.front().second << "\n";
 			queue.pop();
 
 			for (int i = 0; i < 8; i++) {
 				pair<int, int> temp;
 				temp.first = position.first + ::move[i].first;
 				temp.second = position.second + ::move[i].second;
-				if (!isVaild(temp, I) || visited.count(temp) == 1)
+				if (!isVaild(temp, I) || graph[temp.first][temp.second] > 0)
 					continue;
 
-				visited.insert(temp);
 				queue.push(temp);
 				if (graph[temp.first][temp.second] == 0 || graph[temp.first][temp.second] >= graph[position.first][position.second] + 1)
 					graph[temp.first][temp.second] = graph[position.first][position.second] + 1;
 			
-
-				if (end.first == temp.first && end.second == temp.second) {
-					state = true;
-				}
 			}
 
 			if (state) break;
 		}	
 		
 		::cout << graph[end.first][end.second] << "\n";
-
-		/*for (int j = 0; j < I; j++) {
-			for (int k = 0; k < I; k++)
-				::cout << graph[j][k] << " ";
-			cout << "\n";
-		}*/
 	}
 	
 
