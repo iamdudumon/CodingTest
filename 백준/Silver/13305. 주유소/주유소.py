670b538130c29ed28ff33823bf669246dee0f5c0
김두현 <102767676+iamdudumon@users.noreply.github.com>
2024-04-01 01:36:20 +0900

    [Silver III] Title: 주유소, Time: 116 ms, Memory: 46236 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -6,16 +6,11 @@ n = int(input())
 distance = list(map(int, input().split()))
 cost = list(map(int, input().split()))
 
-min_cost = 0
-i = 0
-while i < n - 1:
-    cur_cost = cost[i]
-    more_cost = 0
-    for j in range(i + 1, n - 1):
-        if cur_cost <= cost[j]:
-            more_cost+=1
-    for j in range(more_cost + 1):
-        min_cost += (cur_cost * distance[i + j])
-    i += (more_cost + 1)
+min_cost = cost[0]
+total_cost = 0
+for i in range(n - 1):
+    if min_cost > cost[i]:
+        min_cost = cost[i]
+    total_cost += (distance[i] * min_cost)
 
-print(min_cost)
+print(total_cost)
