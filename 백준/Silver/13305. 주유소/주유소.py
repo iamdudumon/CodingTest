9d5b5b2a63dbaa4cd1e2a115465ae09c76601fbe
김두현 <102767676+iamdudumon@users.noreply.github.com>
2024-04-01 01:07:29 +0900

    [Silver III] Title: 주유소, Time: 112 ms, Memory: 41844 KB, Score: 17 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -11,8 +11,8 @@ i = 0
 while i < n - 1:
     cur_cost = cost[i]
     more_cost = 0
-    for j in range(i + 1, n):
-        if cur_cost < cost[j]:
+    for j in range(i + 1, n - 1):
+        if cur_cost <= cost[j]:
             more_cost+=1
     for j in range(more_cost + 1):
         min_cost += (cur_cost * distance[i + j])
