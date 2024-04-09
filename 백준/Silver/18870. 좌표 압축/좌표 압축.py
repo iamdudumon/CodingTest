450ec77afc001cfce8cf1fc0f02afdd5ddf6214f
김두현 <102767676+iamdudumon@users.noreply.github.com>
2024-04-10 00:31:17 +0900

    [Silver II] Title: 좌표 압축, Time: 1016 ms, Memory: 291472 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -5,7 +5,6 @@ write = stdout.write
 
 n = int(input())
 xi = list(map(int, input().split()))
-sort_xi = sorted(set(xi))
-xi_map = {x : i for i, x in enumerate(sort_xi)}
+xi_map = {x : i for i, x in enumerate(sorted(set(xi)))}
 answer = [str(xi_map[x]) for x in xi]
 write(" ".join(answer))
