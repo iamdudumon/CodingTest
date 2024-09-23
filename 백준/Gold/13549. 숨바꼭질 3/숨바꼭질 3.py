57d37b841c73f5018c025ce0a5262739d12f3011
김두현 <102767676+iamdudumon@users.noreply.github.com>
2024-09-23 15:56:43 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 168 ms, Memory: 35312 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+visited = [1000000] * 100001
+visited[N] = 0
+
+def move(n, i):
+    if i == 0:
+        return n * 2
+    elif i == 1:
+        return n - 1
+    else:
+        return n + 1
+
+def bfs(N):
+	deq = deque()
+	deq.append((N, 0))
+
+	while deq:
+		num, time = deq.popleft()
+
+		for i in range(3):
+			next = move(num, i)
+			if 0 <= next <= 100000 and visited[next] == 1000000:
+				if i == 0:
+						visited[next] = min(visited[next], time)
+						deq.appendleft((next, visited[next]))
+				else:
+						visited[next] = min(visited[next], time + 1)
+						deq.append((next, visited[next]))
+
+bfs(N)
+print(visited[K])
