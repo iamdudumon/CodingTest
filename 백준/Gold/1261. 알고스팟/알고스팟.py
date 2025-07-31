98be21e6b33d6502d1311fbd2150a0999bac34f0
김두현 <102767676+iamdudumon@users.noreply.github.com>
2025-07-31 14:34:36 +0900

    [Gold IV] Title: 알고스팟, Time: 76 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.py
+++ 백준/Gold/1261. 알고스팟/알고스팟.py
@@ -5,8 +5,8 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 matrix = [list(map(int, input().rstrip())) for _ in range(M)]
-dp = [[0] * N for _ in range(M)]
-visited = [[0] * N for _ in range(M)]
+visited = [[10001] * N for _ in range(M)]
+visited[0][0] = 0
 
 deq = deque()
 deq.append((0, 0))
@@ -16,14 +16,12 @@ while deq:
 
 	for m in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
 		next = (pos[0] + m[0], pos[1] + m[1])
-		if (0 <= next[0] < M) and (0 <= next[1] < N) and visited[next[0]][next[1]] == 0:
+		if (0 <= next[0] < M) and (0 <= next[1] < N) and visited[next[0]][next[1]] == 10001:
 			if matrix[next[0]][next[1]]:
-				dp[next[0]][next[1]] = dp[pos[0]][pos[1]] + 1
+				visited[next[0]][next[1]] = visited[pos[0]][pos[1]] + 1
 				deq.append(next)
 			else:
-				dp[next[0]][next[1]] = dp[pos[0]][pos[1]]
+				visited[next[0]][next[1]] = visited[pos[0]][pos[1]]
 				deq.appendleft(next)
 
-			visited[next[0]][next[1]] = 1
-
-print(dp[M - 1][N - 1])
+print(visited[M - 1][N - 1])
