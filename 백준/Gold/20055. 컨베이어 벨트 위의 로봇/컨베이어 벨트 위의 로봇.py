3171160594cc7b9b1b1795308b831eae51e8ab58
김두현 <102767676+iamdudumon@users.noreply.github.com>
2025-09-09 16:59:15 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 4392 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -0,0 +1,40 @@
+import sys
+
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+robots = [0] * N
+
+start = 0
+stage = 0
+while True:
+    stage+=1
+
+    start = (start - 1) % (2 * N)
+    robots = [False] + robots[:-1]
+    robots[-1] = False
+
+    for i in range(N - 1, -1, -1):
+        next = (start + 1 + i) % (2 * N)
+        if robots[i]:
+            if i == N - 1:
+                robots[i] = 0
+            else:
+                if A[next] and robots[i + 1] == 0:
+                    robots[i] = 0
+                    robots[i + 1] = 1
+                    A[next]-=1
+                    if A[next] == 0:
+                        K-=1
+
+    if A[start]:
+        A[start]-=1
+        if A[start] == 0:
+            K-=1
+        robots[0] = 1
+
+    if K <= 0:
+        break
+
+print(stage)
