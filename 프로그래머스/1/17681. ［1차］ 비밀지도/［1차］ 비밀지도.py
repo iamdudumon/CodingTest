81ba6349df09a5d5cc6403abac51eb36feac866e
김두현 <102767676+iamdudumon@users.noreply.github.com>
2025-09-17 15:21:19 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.12 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -8,16 +8,14 @@ def solution(n, arr1, arr2):
     answer = []
     
     for i in range(n):
-        bin = list(binary(arr1[i] | arr2[i]))
+        bin = list(binary(arr1[i] | arr2[i]).rjust(n, '0'))
         temp = ""
         
-        j = len(bin) - 1
         for ii in range(n - 1, -1, -1):
-            if bin[j] == '1' and j >= 0:
+            if bin[ii] == '1':
                 temp = '#' + temp
             else:
                 temp = ' ' + temp
-            j-=1
                 
         answer.append(temp)
     
