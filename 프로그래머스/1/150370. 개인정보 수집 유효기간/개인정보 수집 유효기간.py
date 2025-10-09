afcf7512ee2c861489f5569a573d03e7fcaa686e
김두현 <102767676+iamdudumon@users.noreply.github.com>
2025-10-10 00:58:51 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.14 ms, Memory: 9.47 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,23 +1,18 @@
-def minus_days(day1, day2):
-    day1 = day1.split(".")
-    day2 = day2.split(".")
-    
-    total_day1 = int(day1[0]) * (12 * 28) + int(day1[1]) * 28 + int(day1[2])
-    total_day2 = int(day2[0]) * (12 * 28) + int(day2[1]) * 28 + int(day2[2].split(' ')[0])
-                     
-    return total_day1 - total_day2
-                     
+def minus_days(today, date):
+    year, mon, day = map(int, date.split("."))                  
+    return today - (year * (12 * 28) + mon * 28 + day)
+
 def solution(today, terms, privacies):
     answer = []
-    
+    todays = today.split(".")
+    today_num = int(todays[0]) * (12 * 28) + int(todays[1]) * 28 + int(todays[2])
     term_dics = {}
     for term in terms:
         temp = term.split(" ")
         term_dics[temp[0]] = int(temp[1])
-    # print(term_dics)
+
     for idx, pri in enumerate(privacies):
-        # print(minus_days(today, pri), term_dics[pri[-1]] * 28)
-        if minus_days(today, pri) >= term_dics[pri[-1]] * 28:
+        if minus_days(today_num, pri[:-1]) >= term_dics[pri[-1]] * 28:
             answer.append(idx + 1)
         
     return answer
