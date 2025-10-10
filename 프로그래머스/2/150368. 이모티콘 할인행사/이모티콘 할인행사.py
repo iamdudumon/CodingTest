75e1e844587a0a8d05b2f9253da3f0a9ba227a6d
김두현 <102767676+iamdudumon@users.noreply.github.com>
2025-10-11 01:54:44 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 1251.19 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
@@ -1,20 +1,21 @@
 def discount(money, dis):
-    # return int(((100 - dis) / 100) * money)
-    return (1 - dis*0.01)*100*money/100;
+    return ((100 - dis) / 100) * money
 
 def solution(users, emoticons):
     answer = [0, 0]
-    combi = [10] * len(emoticons)
+    combis = [10] * len(emoticons)
+    end_flag = len(emoticons) * 40
     
     while True:
         plus = cost = 0
-
+        price = [discount(emoticons[i], combis[i]) for i in range(len(emoticons))]
+        
         for user in users:
             user_total = 0
-            for idx, co in enumerate(combi):
-                if user[0] > co:
+            for idx, dis in enumerate(combis):
+                if user[0] > dis:
                     continue
-                user_total += discount(emoticons[idx], co)
+                user_total += price[idx]
                 if user_total >= user[1]:
                     break
             if user_total >= user[1]:
@@ -27,16 +28,15 @@ def solution(users, emoticons):
             answer[1] = cost
         elif answer[0] == plus:
             answer[1] = max(answer[1], cost)
-            
         
-        if sum(combi) == len(emoticons) * 40:
+        if sum(combis) == end_flag:
             break
-        
+    
         i = 0
         while i < len(emoticons):
-            combi[i] += 10
-            if combi[i] == 50:
-                combi[i] = 10
+            combis[i] += 10
+            if combis[i] == 50:
+                combis[i] = 10
                 i += 1
             else:
                 break
